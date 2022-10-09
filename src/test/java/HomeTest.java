d09583ca94f78d89d1d561d78efb435384cdeff6
heyitsaqua <offbeattsunami@gmail.com>
2022-10-09 09:31:46 -0400

    US43 completed

--- src/test/java/HomeTest.java
+++ src/test/java/HomeTest.java
@@ -6,8 +6,15 @@ public class HomeTest {
     int id = 99;
     String email = "aidar@blank.com";
 
-    @Test(testName = "Testing title of the page")
+    @Test(testName = "US34: Testing title of the page")
     public void test01() {
         //implementation
+    }
+
+    @Test(testName = "US43: Testing employee input table")
+    public void test02() {
+        //implementation part 1
+        //use switchToNewWindow method
+        //completed test case
     }
 }
