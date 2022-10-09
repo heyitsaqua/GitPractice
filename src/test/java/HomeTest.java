6cc6998368a1f92efdf4a01af77fbf5f8dba059f
heyitsaqua <offbeattsunami@gmail.com>
2022-10-09 09:25:46 -0400

    US34 completed

--- src/test/java/HomeTest.java
+++ src/test/java/HomeTest.java
@@ -1,6 +1,13 @@
+import org.testng.annotations.Test;
+
 public class HomeTest {
     String name = "Aidar";
     String role = "Student";
     int id = 99;
     String email = "aidar@blank.com";
+
+    @Test(testName = "Testing title of the page")
+    public void test01() {
+        //implementation
+    }
 }
